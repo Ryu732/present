6f92b53d39cab571e9f1924f678526323a3dece6
Ryu732 <sakanaction001@gmail.com>
2024-12-03 20:22:17 +0900

    基本機能完成

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<v-app>
+		<v-main>
+			<v-container>
+				<div class="d-flex align-center justify-center fill-height">
+					<v-btn color="primary">
+						<v-icon>mdi-power</v-icon>
+						カワウソ認証システムを起動
+						<v-dialog v-model="isKawauso" activator="parent">
+							<v-card prepend-icon="mdi-shield-check" title="カワウソ認証">
+								<v-divider thickness="1"></v-divider>
+								<v-card-text>
+									カワウソ認証が必要です。<br />
+									写真のカワウソの名前を入力してください。
+									<v-img :src="imgUsosan" />
+									<v-spacer />
+									<v-text-field v-model="kawausoInput" label="カワウソの名前" />
+								</v-card-text>
+								<v-card-actions>
+									<v-btn @click="kawausoAuth" color="primary">送信</v-btn>
+								</v-card-actions>
+							</v-card>
+						</v-dialog>
+					</v-btn>
+					<v-dialog v-model="isAuth" persistent>
+						<v-card>
+							<v-card-title>認証成功!</v-card-title>
+							<v-card-text>
+								おめでとうございます<br />
+								ばなながもらえます。
+								<v-img :src="imgBanana" />
+							</v-card-text>
+						</v-card>
+					</v-dialog>
+				</div>
+			</v-container>
+		</v-main>
+	</v-app>
+
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import { ref } from 'vue';
+const imgUsosan = require('./assets/usosan.jpg');
+const imgBanana = require('./assets/banana.jpg');
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const isKawauso = ref<boolean>(false);
+const isAuth = ref<boolean>(false);
+const kawausoName = ref<string>('ウソさん');
+const kawausoInput = ref<string>('');
+
+function kawausoAuth() {
+	if (kawausoName.value === kawausoInput.value) {
+		isKawauso.value = false;
+		isAuth.value = true;
+	} else {
+		alert('名前が違います。\nひらがなとカタカナに注意して入力してください。');
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.v-container {
+	height: 100%;
 }
 </style>
